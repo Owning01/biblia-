893e5c0c57d159cebc0b8e8b492230da132be640
Octavio Gonzalez <octaviofacultad@gmail.com>
2026-07-07 03:21:24 -0300

    fix: excluye claves de Firebase de git, agrega template

---
+++ lib/firebase_options.template.dart
@@ -0,0 +1,42 @@
+// Template for firebase_options.dart
+// Copy this file to firebase_options.dart and fill in your Firebase config.
+// Run: flutterfire configure --project=YOUR_PROJECT_ID
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError('Web not configured. Run flutterfire configure.');
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions not configured for $defaultTargetPlatform.',
+        );
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'YOUR_API_KEY',
+    appId: 'YOUR_APP_ID',
+    messagingSenderId: 'YOUR_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_PROJECT_ID.firebasestorage.app',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'YOUR_IOS_API_KEY',
+    appId: 'YOUR_IOS_APP_ID',
+    messagingSenderId: 'YOUR_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_PROJECT_ID.firebasestorage.app',
+    iosClientId: 'YOUR_IOS_CLIENT_ID',
+    iosBundleId: 'com.oky.biblia',
+  );
+}
